4401ab429691cc5d19107dc46c8b090b50936a10
Ewi Benard <ewiben6000@gmail.com>
2022-09-05 04:00:15 +0100

    complex numbers

---
+++ complexnumbers/complexnumbers.vb
@@ -0,0 +1,29 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim x1 As Integer
+        Dim x2 As Integer
+        Dim y1 As Integer
+        Dim y2 As Integer
+        Dim y3 As Integer
+        Dim x3 As Integer
+
+
+        Console.WriteLine("ENTER THE VALUE OF x1")
+        x1 = Console.ReadLine()
+        Console.WriteLine("ENTER THE VALUE OF x2")
+        x2 = Console.ReadLine()
+        Console.WriteLine("ENTER THE VALUE OF y1")
+        y1 = Console.ReadLine()
+        Console.WriteLine("ENTER THE VALUE OF y2")
+        y2 = Console.ReadLine()
+
+        x3 = (x1 + x2)
+        y3 = (y1 + y2)
+
+        Console.Write("the new complex number is: ")
+        Console.Write("i")
+
+    End Sub
+End Module
